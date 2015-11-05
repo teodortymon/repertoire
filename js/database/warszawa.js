744448c777ed62a657b475462629fcfe317218cb
Teodor Wójcik <teodor.wojcik@gmail.com>
2015-11-05 16:25:47 +0100

    Update warszawa.js

--- js/database/warszawa.js
+++ js/database/warszawa.js
@@ -1,29 +1,29 @@
 var warszawa = {
     'theatresEvents': { 
         "narodowy": [
-            { date: '2014-09-04 15:30', title: 'Big Premiera', theatre: 'narodowy'  },
-            { date: '2014-09-12 10:00', title: 'dfsdf',  theatre: 'narodowy' },
-            { date: '2014-09-24 18:40', title: 'fsdfsd',  theatre: 'narodowy' },
+            { date: '2015-11-04 15:30', title: 'Big Premiera', theatre: 'narodowy'  },
+            { date: '2015-11-12 10:00', title: 'Kotka na blaszanym dachu',  theatre: 'narodowy' },
+            { date: '2015-11-24 18:40', title: 'Malecki',  theatre: 'narodowy' },
         ],
         
         "powszechny": [
-            { date: '2014-09-12 18:30', title: 'AAA', theatre: 'powszechny'},
-            { date: '2014-09-13 18:30', title: 'AAA', theatre: 'powszechny'},
-            { date: '2014-09-14 18:30', title: 'AAA', theatre: 'powszechny'},
+            { date: '2015-11-12 18:30', title: 'Spekatkl 1', theatre: 'powszechny'},
+            { date: '2015-11-13 18:30', title: 'Probny', theatre: 'powszechny'},
+            { date: '2015-11-14 18:30', title: 'Gombrowicz', theatre: 'powszechny'},
         ],
         
         "wspolczesny": [
-            { date: '2014-09-12 18:30', title: 'BBB', theatre: 'wspolczesny'},
-            { date: '2014-09-22 18:30', title: 'BBB', theatre: 'wspolczesny'},
-            { date: '2014-09-23 18:30', title: 'BBB', theatre: 'wspolczesny'},
-            { date: '2014-09-24 18:30', title: 'BBB', theatre: 'wspolczesny'},
+            { date: '2015-11-12 18:30', title: 'Przecietny', theatre: 'wspolczesny'},
+            { date: '2015-11-22 18:30', title: 'Słaby', theatre: 'wspolczesny'},
+            { date: '2015-11-23 18:30', title: 'Do kitu', theatre: 'wspolczesny'},
+            { date: '2015-11-24 18:30', title: 'Cienko cienko', theatre: 'wspolczesny'},
         ],
         
         "tr": [
-            { date: '2014-09-12 18:30', title: 'CCC', theatre: 'tr'},
-            { date: '2014-09-22 18:30', title: 'CCC', theatre: 'tr'},
-            { date: '2014-09-24 18:30', title: 'CCC', theatre: 'tr'},
-            { date: '2014-09-26 18:30', title: 'CCC', theatre: 'tr'},
+            { date: '2015-11-12 18:30', title: 'Nie chcesz na to iść', theatre: 'tr'},
+            { date: '2015-11-22 18:30', title: 'A na to bardzo bardzo chcesz', theatre: 'tr'},
+            { date: '2015-11-24 18:30', title: 'Tu wepchnąłem się ze swoją kobietą', theatre: 'tr'},
+            { date: '2015-11-26 18:30', title: 'A tu ze swoim kumplem', theatre: 'tr'},
         ],
     },
             
