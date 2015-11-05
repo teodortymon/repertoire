c5e33f7f6ed9d5d2af03c2d2dd4bcfb36f26517c
Teodor Wójcik <teodor.wojcik@gmail.com>
2015-11-05 16:26:20 +0100

    Update krakow.js

--- js/database/krakow.js
+++ js/database/krakow.js
@@ -1,15 +1,15 @@
 var krakow = {
     'theatresEvents': { 
         "teatr1": [
-            { date: '2014-09-04 15:30', title: 'BAdadasafd', theatre: 'teatr1'  },
-            { date: '2014-09-12 10:00', title: 'dfsdf',  theatre: 'teatr1' },
-            { date: '2014-09-24 18:40', title: 'fsdfsd',  theatre: 'teatr1' },
+            { date: '2015-11-04 15:30', title: 'BAdadasafd', theatre: 'teatr1'  },
+            { date: '2015-11-12 10:00', title: 'dfsdf',  theatre: 'teatr1' },
+            { date: '2015-11-24 18:40', title: 'fsdfsd',  theatre: 'teatr1' },
         ],
         
         "teatr2": [
-            { date: '2014-09-12 18:30', title: 'AAA', theatre: 'teatr2'},
-            { date: '2014-09-13 18:30', title: 'AAA', theatre: 'teatr2'},
-            { date: '2014-09-14 18:30', title: 'AAA', theatre: 'teatr2'},
+            { date: '2015-11-12 18:30', title: 'AAA', theatre: 'teatr2'},
+            { date: '2015-11-13 18:30', title: 'AAA', theatre: 'teatr2'},
+            { date: '2015-11-14 18:30', title: 'AAA', theatre: 'teatr2'},
         ]
     },
             
